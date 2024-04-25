c04559f0e2190835b7f5c2e5a2fd947a881816e7
Anthony Rohloff <anthony.rohloff@gmail.com>
2024-04-24 20:08:42 -0400

    Added database creation automation and first-time setup flow

--- passbox.py
+++ passbox.py
@@ -1,36 +1,100 @@
 import sqlite3
+import getpass
+import os
 
+# Only skip login after first-time setup
+skip_login = False
+
+# Database file path
+db = "credentials.db"
+
+# Check if the database file exists
+if not os.path.exists(db):
+    # If the database file doesn't exist, create it and perform initial setup
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+
+    # Create all necessary tables
+    cursor.execute("CREATE TABLE setup(master TEXT)")
+    cursor.execute("CREATE TABLE entries(id INTEGER PRIMARY KEY AUTOINCREMENT, service TEXT, username TEXT, password TEXT)")
+    connection.commit()
+
+    # Set master password
+    setup_loop = True
+    while setup_loop:
+        # User enters password twice
+        password1 = getpass.getpass("\nEnter master password: ")
+        password2 = getpass.getpass("\nRe-enter master password: ")
+        
+        # If passwords match, set master password
+        if password1 == password2:
+            cursor.execute("INSERT INTO setup (master) VALUES (?)", (password1,))
+            connection.commit()
+            setup_loop = False
+            skip_login = True
+        # Else, try again
+        else:
+            print("Passwords do not match!\nTry again\n")
+            continue
+
+    # Close the connection
+    cursor.close()
+    connection.close()
+
+# Login loop
+login = False
+while not login and not skip_login:
+    master = getpass.getpass("\nEnter master password: ")
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    cursor.execute("SELECT master FROM setup")
+    fetched_master = cursor.fetchone()
+    
+    # If entered password matches db, access granted
+    if fetched_master and master == fetched_master[0]:
+        print(("Access Granted"))
+        login = True
+    # Else, try again
+    else:
+        print("Access Denied\nTry again")
+    
+    # Close the connection
+    cursor.close()
+    connection.close()
+
+# Main loop
 run = True
-
 while run:
     print("\n\n\nMenu:\nSelect Action")
     action = int(input("[1] Enter a new username and password\n[2] Browse entries\n[3] Remove an entry\n[4] Quit\n"))
     
+    # Enter a new entry
     if action==1:
         print("Create new entry:\nPress enter to return to menu\n")
-        purpose = input("Input entry name:\n")
+        service = input("Input entry name:\n")
         
-        if purpose == "":
+        if service == "":
             continue
         
         username = input("\nInput username:\n")
         password = input("\nInput password:\n")
         
-        connection = sqlite3.connect("data.db")
+        connection = sqlite3.connect(db)
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO entries (purpose, username, password) VALUES (?, ?, ?)", (purpose, username, password))
+        cursor.execute("INSERT INTO entries (service, username, password) VALUES (?, ?, ?)", (service, username, password))
 
         
         connection.commit()
         cursor.close()
         connection.close()
+    # Browse entries
     elif action==2:
         print("Select entry to view by entering id:\nPress enter to return to menu\n")
         
-        connection = sqlite3.connect("data.db")
+        connection = sqlite3.connect(db)
         cursor = connection.cursor()
         
-        for row in cursor.execute("SELECT id, purpose FROM entries"):
+        for row in cursor.execute("SELECT id, service FROM entries"):
             print(f"[{row[0]}] {row[1]}")
         selection = input("\n")
         
@@ -46,19 +110,20 @@ while run:
             print("Username:", selected_entry[2])
             print("Password:", selected_entry[3])
         else:
-            print("Entry with purpose '{}' not found.".format(selection))
+            print("Entry with service '{}' not found.".format(selection))
         
         cursor.close()
         connection.close()
         
         input("\nPress enter to continue")
+    # Remove an entry
     elif action ==3:
         print("\nSelect entry to remove by entering id:\nPress enter to return to menu\n1")
         
-        connection = sqlite3.connect("data.db")
+        connection = sqlite3.connect(db)
         cursor = connection.cursor()
         
-        for row in cursor.execute("SELECT id, purpose FROM entries"):
+        for row in cursor.execute("SELECT id, service FROM entries"):
             print(f"[{row[0]}] {row[1]}")
         selection = input("\n")     
         
@@ -72,6 +137,6 @@ while run:
         connection.close()      
         
         input("Entry removed\nPress enter to continue")
+    # Quit
     else:
         run=False
-
